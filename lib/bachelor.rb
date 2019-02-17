dd6ee4c128afa5fb1189b4d7533447615f20c776
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-17 19:57:11 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+def get_first_name_of_season_winner(data, season){
+  :data => {
+    "season": "winter"
+    "wheather": "cold"
+  }
+}
+  
 end
 
 def get_contestant_name(data, occupation)
